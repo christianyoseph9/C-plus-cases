9b3fa6e8d38f3625c8385fb2addc2a021fe9a455
christianyoseph9 <63234711+christianyoseph9@users.noreply.github.com>
2021-01-14 22:39:10 +0700

    Add files via upload

---
+++ First Try C++.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num;
+
+    num = 6;
+
+    cout <<"My first C++" << endl;
+    cout <<"2 + 8 = "<< 2 + 8 <<endl;
+    cout <<"Num =" << num << endl;
+
+    return 0;
+}
+
+
