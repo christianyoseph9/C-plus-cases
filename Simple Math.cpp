522b602fafcbcba9c23ae3e9da160bacd8e3a24a
Yoseph Christian <63234711+christianyoseph9@users.noreply.github.com>
2021-01-16 23:15:53 +0700

    Add files via upload

---
+++ Simple Math.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "3 + 4 ="   << 3+4 << endl;
+    cout << "5 - 6 ="   << 5-6 << endl;
+    cout << "10 * 6 ="  << 10*6 <<endl;
+    cout << "5 / 2 ="   << 5/2 <<endl;
+    cout << "5.0 / 2.0 =" << 5.0/2.0 <<endl;
+    cout << "6 % 4 ="   << 6%4 <<endl;
+
+    return 0;
+
+}
