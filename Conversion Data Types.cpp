04d2fa116788482a10b032a2189bedabfe2c7b51
Yoseph Christian <63234711+christianyoseph9@users.noreply.github.com>
2021-01-17 23:06:06 +0700

    Add files via upload

---
+++ Conversion Data Types.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+cout << static_cast <int> (15 + static_cast <int> (2.0 + 5.3)) <<endl;
+cout << static_cast <double> (7);
+
+return 0;
+}
